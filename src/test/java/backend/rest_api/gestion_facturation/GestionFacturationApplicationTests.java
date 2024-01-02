1c2e289b4bb63b6c961a9e4a9a5892410c1f49e0
MtuImara <92975795+MtuImara@users.noreply.github.com>
2024-01-02 17:44:43 +0200

    premier commit

---
+++ src/test/java/backend/rest_api/gestion_facturation/GestionFacturationApplicationTests.java
@@ -0,0 +1,13 @@
+package backend.rest_api.gestion_facturation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestionFacturationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
