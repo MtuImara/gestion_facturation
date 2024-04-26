00bc4a023c0052e8e60e647398768a4752a51d4f
MtuImara <92975795+MtuImara@users.noreply.github.com>
2024-04-26 17:40:00 +0200

    Rapport Facture

--- src/main/java/backend/rest_api/gestion_facturation/rapports/controller/FactureRapportController.java
+++ src/main/java/backend/rest_api/gestion_facturation/rapports/controller/FactureRapportController.java
@@ -1,5 +1,41 @@
 package backend.rest_api.gestion_facturation.rapports.controller;
 
+import java.util.Map;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import backend.rest_api.gestion_facturation.helpers.MessageHelper;
+import backend.rest_api.gestion_facturation.helpers.ResponseHelper;
+import backend.rest_api.gestion_facturation.rapports.service.FactureRapportService;
+import lombok.RequiredArgsConstructor;
+
+@CrossOrigin(origins = "*")
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/rapport_facture")
 public class FactureRapportController {
-    
+
+    private final FactureRapportService factureRapportService;
+
+    @RequestMapping(value = "/", method = RequestMethod.GET)
+    public ResponseEntity<?> getEtatSaisieInventaireStandard(@RequestParam(required = false) Integer idArticle) {
+
+        Map<String, Object> data = factureRapportService.getEtatFactureService();
+
+        if (data.size() > 0) {
+            return new ResponseEntity<>(new ResponseHelper(MessageHelper.success(),
+                    data, true), HttpStatus.OK);
+        } else {
+            return new ResponseEntity<>(new ResponseHelper(
+                    MessageHelper.noContent(), false), HttpStatus.OK);
+        }
+
+    }
+
 }
--- src/main/java/backend/rest_api/gestion_facturation/rapports/service/FactureRapportService.java
+++ src/main/java/backend/rest_api/gestion_facturation/rapports/service/FactureRapportService.java
@@ -1,11 +1,13 @@
 package backend.rest_api.gestion_facturation.rapports.service;
 
+import java.math.BigDecimal;
 import java.util.ArrayList;
 import java.util.LinkedHashMap;
 import java.util.List;
 
 import org.springframework.stereotype.Service;
 
+import backend.rest_api.gestion_facturation.gestionClient.mapper.ClientMapper;
 import backend.rest_api.gestion_facturation.gestionFacture.entity.FactureDetailEntity;
 import backend.rest_api.gestion_facturation.gestionFacture.entity.FactureEntity;
 import backend.rest_api.gestion_facturation.gestionFacture.repository.FactureDetailRepository;
@@ -21,7 +23,41 @@ public class FactureRapportService {
         private final FactureDetailRepository factureDetailRepository;
         private final FactureService factureService;
 
-        public LinkedHashMap<String, Object> getEtatInventaireService() {
+        private List<LinkedHashMap<String, Object>> getlistesDetailFacture(
+                        List<FactureDetailEntity> entityList) {
+                LinkedHashMap<String, Object> hashMap = null;
+                List<FactureDetailEntity> entities = entityList;
+                List<LinkedHashMap<String, Object>> hashMaps = null;
+                hashMaps = new ArrayList<>();
+
+                for (FactureDetailEntity each : entities) {
+
+                        hashMap = new LinkedHashMap<>();
+                        hashMap.put("facture", each.getFacture());
+                        hashMap.put("client", ClientMapper.getInstance()
+                                        .convertToDto(each.getFacture().getClient()));
+                        hashMap.put("detailService", each.getServiceDetail().getDesignation());
+                        hashMap.put("quantite", each.getQuantite());
+                        hashMap.put("prix", each.getPrixUnitHt());
+                        hashMap.put("tva", each.getTauxTva());
+                        hashMap.put("prixTotal", each.getPrixTotal());
+
+                        if (factureDetailRepository
+                                        .montantTotalFactureHT(each.getIdFacture()) != null
+                                        || factureDetailRepository.montantTotalFactureHT(
+                                                        each.getIdFacture()) == 0) {
+                                hashMap.put("montantTotal", new BigDecimal(factureDetailRepository
+                                                .montantTotalFactureHT(each.getIdFacture())));
+                        } else {
+                                hashMap.put("montantTotal", 0.0);
+                        }
+
+                        hashMaps.add(hashMap);
+                }
+                return hashMaps;
+        }
+
+        public LinkedHashMap<String, Object> getEtatFactureService() {
 
                 LinkedHashMap<String, Object> hashMap = new LinkedHashMap<>();
 
@@ -39,23 +75,23 @@ public class FactureRapportService {
                                         .getByFacture(saisieInventaireEntity.getId());
 
                         if (saisieInventaireDetail.size() > 0) {
-                                hashMap1.put("code", saisieInventaireEntity.getCode());
-                                hashMap1.put("service", saisieInventaireEntity.getService().getDesignation());
-                                hashMap1.put("etat", saisieInventaireEntity.getEtat());
-                                hashMap1.put("commentaire", saisieInventaireEntity.getCommentaire());
-                                hashMap1.put("dateOperation", saisieInventaireEntity.getDateOperation());
-                                hashMap1.put("dateEcheance", saisieInventaireEntity.getDateEcheance());
-                                // hashMap1.put("dateSaisieInventaire",
-                                // saisieInventaireEntity.getDateSaisieInventaire());
+                                // hashMap1.put("code", saisieInventaireEntity.getCode());
+                                // hashMap1.put("client", ClientMapper.getInstance()
+                                // .convertToDto(saisieInventaireEntity.getClient()));
+                                // hashMap1.put("service",
+                                // saisieInventaireEntity.getService().getDesignation());
+                                // hashMap1.put("etat", saisieInventaireEntity.getEtat());
+                                // hashMap1.put("commentaire", saisieInventaireEntity.getCommentaire());
+                                // hashMap1.put("dateOperation", saisieInventaireEntity.getDateOperation());
+                                // hashMap1.put("dateEcheance", saisieInventaireEntity.getDateEcheance());
 
-                                // hashMap1.put("factureDetail",
-                                // getlistesDetailSaisieInventaire(saisieInventaireDetail));
+                                hashMap1.put("factureDetail", getlistesDetailFacture(saisieInventaireDetail));
 
                                 maps.add(hashMap1);
                         }
                 }
 
-                hashMap.put("mouvementsDeStock", maps);
+                hashMap.put("rapportFacture", maps);
 
                 return hashMap;
         }
